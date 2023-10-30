16d8803dfdfc919638f131959fc2572bb7d963cf
Alessandro Venanzi <alevena00@gmail.com>
2023-10-30 15:15:05 +0100

    Leo work

---
+++ main/Dataset_1.R
@@ -0,0 +1,103 @@
+setwd("C:/Users/leoma/OneDrive/Documents/PoliMi/Bayesian statistics/Progetto_Locale/dati_ozono")
+
+library(lubridate)
+library(rstan)
+library(dplyr)
+library(tidyr)
+
+ozono = read.csv("datasetO3.csv")
+stazioni = read.csv("stazioni_O3.csv")
+ozono$idOperatore = NULL
+stazioni.usate <- stazioni[which(stazioni$IdSensore %in% unique(ozono$idSensore)),]
+start.stop <- data.frame(stazioni.usate$DataStart, stazioni.usate$DataStop)
+
+started.late <- start.stop[which(as.Date(start.stop$stazioni.usate.DataStart, format = "%d/%m/%Y") > 
+                                   as.Date("01/01/2010", format = "%d/%m/%Y")),] 
+
+closed.early <- start.stop[which(as.Date(start.stop$stazioni.usate.DataStop, format = "%d/%m/%Y") > 
+                                   as.Date("01/01/2010", format = "%d/%m/%Y")),] 
+ozono$Data <- mdy_hms(ozono$Data)
+ozono$Year <- year(ozono$Data-1)    
+ozono$Month <- month(ozono$Data-1)
+ozono$Day <- day(ozono$Data-1)    
+ozono$Hour <- hour(ozono$Data-1)  
+
+sapply(ozono, function(y) sum(length(which(is.na(y)))))
+
+counts <- NULL
+sensors <- unique(ozono$idSensore)
+years <- unique((ozono$Year))
+mesi <- 5:10
+#We have to consider only from 5 to 10 in the month
+giorni_true <- c(31, 30, 31, 31, 30, 31)
+lista_sensori <- list()
+for (i in 1:length(sensors))
+{
+  lista_sensori[[i]] <- ozono[which(ozono$idSensore==sensors[i]) ,]
+}
+
+for (i in 1:length(lista_sensori))
+{
+  temp <- lista_sensori[[i]]
+  lista_anni <- list()
+  for (j in 1:length(years))
+  {
+    lista_anni[[j]] <- temp[which(temp$Year == years[j]) ,]
+  }
+  
+  for (j in 1:length(years))
+  {
+    temp_year <- lista_anni[[j]]
+    lista_mesi <- list()
+    for (k in 1:length(mesi))
+    {
+      lista_mesi[[k]] <- temp_year[which(temp_year$Month==mesi[k]) ,]
+    }
+    
+    for (k in 1:length(mesi))
+    {
+      temp_mese <- lista_mesi[[k]]
+      giorni <- unique(lista_mesi[[k]]$Day)
+      lista_giorni <- list()
+      
+      if(length(giorni))
+      {
+        miss <- setdiff(1:giorni_true[k], giorni)
+        for (d in 1:length(giorni))
+        {
+          lista_giorni[[d]] <- temp_mese[which(temp_mese$Day==giorni[d]) ,]
+        }
+        count <- NULL
+        na <- NULL
+        for (d in 1:length(giorni))
+        {
+          na[d] <- sum(is.na(lista_giorni[[d]]$Valore))
+          count[d] <- sum(na.omit(lista_giorni[[d]]$Valore)>180)>0
+        }
+        counts <- rbind(counts, c(sum(count), sensors[i], years[j], mesi[k], length(miss), sum(na)))
+      }
+      else
+      {
+        print(c(sensors[i], mesi[k], years[j]))
+      }
+    }
+  }
+}
+
+counts <- data.frame(counts)
+colnames(counts) <- c("Count", "idSensore", "Year", "Month", "#Missing days", "Nas on 720 obs.")
+
+#Mancano dei giorni, il codice dovrà tenerne conto, facciamo media (?) ma successiva, prima vediamo se funziona
+#Mancano delle righe, verranno ttrattate come Nas, infatti se queste dovessero essere >180 allora assumiamo che una
+#tra quella prima o quella dopo sforino la soglia.
+
+#Commenti su NAs:
+sum(counts$`#Missing days`>0)
+hist(counts$`#Missing days`[which(counts$`#Missing days`>0)])
+    #Mancano nel 10% dei mesi almeno un giorno. Quando ne manca uno ne mancano in media 2.2. Un'idea è stimare quanti
+    #producano effetti in base agli altri quando ne mancano e.g. <5
+sum((counts$`Nas on 720 obs.`>0))
+hist(counts$`Nas on 720 obs.`[which(counts$`Nas on 720 obs.`>0)])
+    #In metà dei mesi del dataset c'è almeno un na, la media quando ce ne è uno è 30 al mese.
+
+#Attenzione che questi due tipi di problemi vanno anche valutati insieme, anche in mesi contigui.
