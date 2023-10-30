5a7d277b8ab4fb5eca1788cefd0d0e3828e8142b
Alessandro Venanzi <alevena00@gmail.com>
2023-10-30 08:37:29 +0100

    pre incontro

--- main/data cleaning.R
+++ main/data cleaning.R
@@ -5,6 +5,7 @@ setwd("C:/Users/39339/OneDrive/Desktop/I semestre/Bayesian_Project/main")
 library(lubridate)
 library(rstan)
 library(dplyr)
+library(tidyr)
 
 # read data
 
@@ -15,57 +16,58 @@ stazioni = read.csv("C:/Users/39339/OneDrive/Desktop/I semestre/dati_ozono/stazi
 
 ozono$idOperatore = NULL
 
-# check NAs
 
-na_count <-sapply(ozono, function(y) sum(length(which(is.na(y)))))
-na_count
-
-100 * na_count[3] / length(ozono[,3])
-
-# 2.376734 % di NAs 
-# da vedere meglio comunque
-
-# converting dates to dmy hms format (-> to deal with am/pm) 
-
-# ozono$Data = lubridate::dmy_hms(ozono$Data)
-
-# per qualche ragione strana crea NAs, da vedere il perchè
-
-# serve preprocessing delle date, magari creando nuove features: ora, giorno, day, 
-# giorno della settimana, mese, anno => uso dplyr + lurbidate per parsare al meglio il formato 
-# e passare da AM/PM a UTC
-
-# in teoria cosi dovrebbe andare, idk why crea tanti NAs
-
-temp = data.frame(ozono)
-
-temp$Data <- dmy_hms(temp$Data) 
-
-temp$Year <- year(temp$Data)    
-temp$Month <- month(temp$Data)
-temp$Day <- day(temp$Data)    
-temp$Hour <- hour(temp$Data)  
-
-# intanto meglio dare un occhio alle stazioni "chiuse temporaneamente"
+# occhio alle stazioni "chiuse temporaneamente"
 
 stazioni.usate <- stazioni[which(stazioni$IdSensore %in% unique(ozono$idSensore)),]
 start.stop <- data.frame(stazioni.usate$DataStart, stazioni.usate$DataStop)
 
 started.late <- start.stop[which(as.Date(start.stop$stazioni.usate.DataStart, format = "%d/%m/%Y") > 
-                   as.Date("01/01/2010", format = "%d/%m/%Y")),] 
+                                   as.Date("01/01/2010", format = "%d/%m/%Y")),] 
 
 closed.early <- start.stop[which(as.Date(start.stop$stazioni.usate.DataStop, format = "%d/%m/%Y") > 
-                                                   as.Date("01/01/2010", format = "%d/%m/%Y")),] 
+                                   as.Date("01/01/2010", format = "%d/%m/%Y")),] 
 
 
 # recap:  8 chiuse prima
 #         1 aperte dopo
 # ne abbiamo 51 totali, forse un po' too much toglierle tutte?
 
-# guardiamo le giornate di utilizzo imho 
+# guardiamo le giornate di utilizzo imho (?) 
 
 
+#### DATA PROCESSING ####
 
+
+# here i change format using lubridate and create Y/M/D/H vars to help future loops
+
+ozono$Data <- mdy_hms(ozono$Data)
+ozono$Year <- year(ozono$Data)    
+ozono$Month <- month(ozono$Data)
+ozono$Day <- day(ozono$Data)    
+ozono$Hour <- hour(ozono$Data)  
+
+sapply(ozono, function(y) sum(length(which(is.na(y)))))
+
+may.oct.15 = ozono[which(ozono$Month %in% 5:10 & ozono$Year %in% 2015),]
+
+new_dataset <- may.oct.15 %>%
+  pivot_wider(names_from = idSensore, values_from = Valore, values_fill = 0)
+
+new_dataset[,c(2,3,4,5,6)] = NULL
+
+data_long <- new_dataset %>%
+  gather(station, value, -Data)
+
+# Create a line plot with different colors for each station
+library(ggplot2)
+
+ggplot(data_long, aes(x = Data, y = value, color = station)) +
+  geom_line() +
+  labs(title = "Time Series for Each Station",
+       x = "Date",
+       y = "Value") +
+  theme_minimal()
 
 
 
