2e8152ef71982e7b2756be984e7f045c367b476a
Alessandro Venanzi <alevena00@gmail.com>
2023-11-08 16:46:21 +0100

    Riorganizzazione cartelle

--- main/Cleaning/data_cleaning_temperature.R
+++ main/Cleaning/data_cleaning_temperature.R
@@ -10,6 +10,7 @@ monthly_means_list <- list()
 
 for (i in 1:51) { # change to 51 later
   
+
 staz = read.csv(paste0("weather/staz", i, ".csv"), skip = 2, header = T)
 staz$time <- ymd(staz$time)
 staz$Year <- year(staz$time)    
@@ -18,16 +19,19 @@ staz$Day <- day(staz$time)
 
 staz <- staz[which(staz$Month %in% 5:10),]
 # Create a new data frame with monthly means
+
+threshold = quantile(staz$windspeed_10m_max..km.h., .75)
+
 monthly.means <- staz %>%
   group_by(Year, Month) %>%
   summarize(
     mean_temperature = mean(temperature_2m_mean...C.),
     mean_precipitation_sum = mean(precipitation_sum..mm.),
     mean_precipitation_hours = mean(precipitation_hours..h.),
-    mean_windspeed_10m_max = mean(windspeed_10m_max..km.h.)
+    wind_count = sum(length(which(windspeed_10m_max..km.h. > threshold)))
   ) %>%
   ungroup()
-
+print(threshold)
 monthly_means_list[[i]] <- monthly.means
 }
 
@@ -49,9 +53,5 @@ for (i in 1:51) {
 }
 
 write.csv(combined_df, "weather_data.csv", row.names=FALSE)
-
-
-
-
 
 
